43c0f28703afa3ecd780480e6d12a9ebd2020ffa
MuthuPandi-B <muthup61@gmail.com>
2025-02-24 13:00:35 +0530

    Completed

---
+++ .history/BackEnd/sockets/chatSocket_20250224124923.js
@@ -0,0 +1,13 @@
+
+
+
+import Message from '../models/Message.js';
+const chatSocket = (io, socket) => {
+  socket.on('sendMessage', async ({ message, userId, recipientId}) => {
+    const chat = new Message({ message, sender: userId, recipient: recipientId });
+    await chat.save();
+    io.emit('receiveMessage', chat);
+  });
+};
+export default chatSocket;
+
---
+++ .history/FrontEnd/src/components/Footer_20250224124519.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const isLoggedIn=localStorage.getItem('token');
+const Footer = ({  userName }) => {
+  return (
+    <footer className="bg-gray-800 text-white p-4 text-center flex justify-center items-center">
+      {isLoggedIn ? (
+        <div>
+        <p>
+          Hello {userName} ! </p>
+          <div className="flex">
+          <Link to={"./terms"} className="hover:underline ml-2 ">Terms & Conditions &bull;</Link>
+          <Link to={"./contact"} className="hover:underline ml-2 ">Contact Us !</Link>
+          </div>
+      <div/>
+      ) : (
+        <p>
+          Welcome to ChatApp! &bull; 
+          <Link to={"./terms"} className="hover:underline ml-2 ">Terms & Conditions &bull;</Link>
+          <Link to={"./contact"} className="hover:underline ml-2 ">Contact Us !</Link>
+        </p>
+      )}
+    </footer>
+  );
+};
+
+export default Footer;
